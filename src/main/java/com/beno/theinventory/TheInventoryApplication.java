d85746d146a6a7683b04818aadad28f8da514a94
Beno <circiubeni@gmail.com>
2025-03-18 17:34:08 +0000

    First commit

---
+++ src/main/java/com/beno/theinventory/TheInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.beno.theinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beno/theinventory/TheInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beno.theinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
